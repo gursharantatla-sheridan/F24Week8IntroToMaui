fc0cace052d19e59529f537004163fcf807e16f5
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-10-28 09:46:26 -0400

    Add project files.

---
+++ F24Week8IntroToMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace F24Week8IntroToMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ F24Week8IntroToMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace F24Week8IntroToMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
